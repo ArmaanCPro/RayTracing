19832c1b68a126051183d14d4353b851c43d482f
Armaan Chahal <armaan.c2009@gmail.com>
2025-04-20 11:45:31 -0500

    Add camera system and convert PerPixel() to TraceRay(), which takes in a Ray struct

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,5 +1,7 @@
 ﻿#include "Renderer.h"
 
+#include "Camera.h"
+#include "Camera.h"
 #include "Walnut/Random.h"
 
 namespace Utils
@@ -34,15 +36,21 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
     m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render()
+void Renderer::Render(const Camera& camera)
 {
+    const glm::vec3& rayOrigin = camera.GetPosition();
+
+    Ray ray;
+    ray.Origin = camera.GetPosition();
+    
     for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
     {
         for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
         {
-            glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-            coord = coord * 2.0f - 1.0f; // -1 -> 1
-            glm::vec4 color = PerPixel(coord);
+            const glm::vec3& rayDirection = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
+            ray.Direction = rayDirection;
+            
+            glm::vec4 color = TraceRay(ray);
             color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
             m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
         }
@@ -51,10 +59,8 @@ void Renderer::Render()
     m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::TraceRay(const Ray& ray)
 {
-    glm::vec3 rayOrigin = glm::vec3(0.0f, 0.0f, 2.0f);
-    glm::vec3 rayDirection = glm::vec3(coord.x, coord.y, -1.0f); // normalizing this tanks performance and isn't strictly necessary
     float radius = 0.5f;
     
     // (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
@@ -64,9 +70,9 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
     // r = radius
     // t = hit distance
 
-    float a = glm::dot(rayDirection, rayDirection);
-    float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-    float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+    float a = glm::dot(ray.Direction, ray.Direction);
+    float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+    float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
     // Discriminant: b^2 - 4ac
     float discriminant = b * b - 4.0f * a * c;
@@ -80,13 +86,12 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
     
     if (tClosest > 0.0f)
     {
-        glm::vec3 hitPoint = rayOrigin + rayDirection * tClosest;
+        glm::vec3 hitPoint = ray.Origin + ray.Direction * tClosest;
         glm::vec3 hitNormal = glm::normalize(hitPoint);
         
         const glm::vec3 lightDirection = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
-        float lightIntensity = glm::dot(hitNormal, -lightDirection);
-        lightIntensity = glm::max(0.0f, lightIntensity);
-        
+        float lightIntensity = glm::max(glm::dot(hitNormal, -lightDirection), 0.1f);
+
         return { m_SphereColor * lightIntensity, 1.0f };
     }
     
--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -9,11 +9,23 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+#include "Camera.h"
+
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
+	ExampleLayer()
+		:
+		m_Camera(45.0f, 0.1f, 100.0f)
+	{}
+	
+	virtual void OnUpdate(float ts) override
+	{
+		m_Camera.OnUpdate(ts);
+	}
+	
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
@@ -47,13 +59,15 @@ public:
 		Timer timer;
 
 		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render();
+		m_Camera.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render(m_Camera);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
 	Renderer m_Renderer;
+	Camera m_Camera;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 	float m_LastRenderTime = 0.0f;
 };
