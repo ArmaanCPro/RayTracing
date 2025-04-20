a882add336042ea5ea60b7413a8f6ccb1b083287
Armaan Chahal <armaan.c2009@gmail.com>
2025-04-20 10:55:50 -0500

    Convert PerPixel to use floats, specifically glm::vec4 rather than uint32_t

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -2,6 +2,19 @@
 
 #include "Walnut/Random.h"
 
+namespace Utils
+{
+    static uint32_t ConvertToRGBA(const glm::vec4& color)
+    {
+        uint8_t r = uint8_t(color.r * 255.0f);
+        uint8_t g = uint8_t(color.g * 255.0f);
+        uint8_t b = uint8_t(color.b * 255.0f);
+        uint8_t a = uint8_t(color.a * 255.0f);
+
+        return (a << 24) | (b << 16) | (g << 8) | r;
+    }
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
     if (m_FinalImage)
@@ -29,17 +42,19 @@ void Renderer::Render()
         {
             glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
             coord = coord * 2.0f - 1.0f; // -1 -> 1
-            m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+            glm::vec4 color = PerPixel(coord);
+            color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+            m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
         }
     }
 
     m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
     glm::vec3 rayOrigin = glm::vec3(0.0f, 0.0f, 2.0f);
-    glm::vec3 rayDirection = glm::normalize(glm::vec3(coord.x, coord.y, -1.0f));
+    glm::vec3 rayDirection = glm::vec3(coord.x, coord.y, -1.0f); // normalizing this tanks performance and isn't strictly necessary
     float radius = 0.5f;
     
     // (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
@@ -58,38 +73,28 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 
     // miss - return black
     if (discriminant < 0.0f)
-        return 0xff000000;
+        return { 0, 0, 0, 1 };
     
-    // tMinus should be the closest hit
-    float tMinus = (-b - glm::sqrt(discriminant)) / (2.0f * a);
-    float tPlus = (-b + glm::sqrt(discriminant)) / (2.0f * a);
-
-    if (tMinus > 0.0f)
+    float tClosest = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+    float tFar = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+    
+    if (tClosest > 0.0f)
     {
-        glm::vec3 hitPoint = rayOrigin + tMinus * rayDirection;
+        glm::vec3 hitPoint = rayOrigin + tClosest * rayDirection;
         glm::vec3 hitNormal = glm::normalize(hitPoint);
         
         const glm::vec3 lightDirection = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
         float lightIntensity = glm::dot(hitNormal, -lightDirection);
         lightIntensity = glm::max(0.1f, lightIntensity);
         
-        uint8_t red = 255;
-        uint8_t green = 0;
-        uint8_t blue = 255;
-        
-        // scaling by intensity
-        red = (uint8_t)(red * lightIntensity);
-        green = (uint8_t)(green * lightIntensity);
-        blue = (uint8_t)(blue * lightIntensity);
-        
-        return 0xff000000 | (red << 16) | (green << 8) | blue;
+        return glm::vec4(glm::vec3(1, 0, 1) * lightIntensity, 1.0f);
     }
     
     // back face
-    else if (tPlus > 0.0f)
+    else if (tFar > 0.0f)
     {
-        return 0xffff00ff;
+        return { 1, 0, 1, 1 };
     }
 
-    return 0xff000000;
+    return { 0, 0, 0, 1 }; // 0xff000000
 }
--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -4,6 +4,11 @@
 #include <memory>
 #include <glm/glm.hpp>
 
+#include "Walnut/Random.h"
+#include "Walnut/Random.h"
+#include "Walnut/Random.h"
+#include "Walnut/Random.h"
+
 class Renderer
 {
 public:
@@ -15,7 +20,7 @@ public:
     std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 
 private:
-    uint32_t PerPixel(glm::vec2 coord);
+    glm::vec4 PerPixel(glm::vec2 coord);
 private:
     std::shared_ptr<Walnut::Image> m_FinalImage;
     uint32_t* m_ImageData = nullptr;
